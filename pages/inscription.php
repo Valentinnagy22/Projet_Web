cbf72430a40f4e15c8a94ac633f709d46fbfb9af
Valentin Nagy <valentin.nagy@hotmail.fr>
2018-01-01 23:57:14 +0100

    Suite

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,1 +1,33 @@
 <h3 id="titre_inscription">Entrez vos données : </h3>
+
+<form action="<?php print $_SERVER['PHP_SELF']; ?>" method="get" id="form_inscription">
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-4">
+            <input type="text" id="nom" name="nom" placeholder="Votre nom"/>
+        </div>
+        <div class="col-sm-4">
+            <input type="text" id="prenom" name="prenom" placeholder="Votre prénom"/>
+        </div>
+    </div>
+    </br>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-4">
+            <input type="email" id="email" name="email" placeholder="Votre email"/>
+        </div>
+        <div class="col-sm-4">
+            <input type="email" id="email2" name="email1" placeholder="Confirmez votre email"/>
+        </div>
+    </div>
+    </br>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-4">
+            <input type="password" id="mdp" name="mdp" placeholder="Votre mot de passe"/>
+        </div>
+        <div class="col-sm-4">
+            <input type="password" id="mdp2" name="mdp2" placeholder="Confirmez votre mdp"/>
+        </div>
+    </div>
+</form>
